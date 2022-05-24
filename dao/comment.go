3fa0aff5cd49054ddc93020bdd473c8e0bc55168
dawnzzz <635441876@qq.com>
2022-05-24 20:27:08 +0800

    change models of favorite and comment

--- dao/comment.go
+++ dao/comment.go
@@ -7,11 +7,12 @@ import (
 
 type Comment struct {
 	ID        uint64         `gorm:"column:id;primary_key;AUTO_INCREMENT"`
+	CommentID uint64         `gorm:"column:comment_id;NOT NULL"`
 	VideoID   uint64         `gorm:"column:video_id;NOT NULL;index"`
 	UserID    uint64         `gorm:"column:user_id;NOT NULL"`
 	Content   string         `gorm:"content:user_id;NOT NULL"`
-	CreatedAt time.Time      `gorm:"column:created_at;autoCreateTime:true;NOT NULL"`
-	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at;autoCreateTime:true;NOT NULL"`
+	CreatedAt time.Time      `gorm:"column:created_at"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at"`
 }
 
 func (Comment) TableName() string {
