868fe9bed3f1f30bd3386cd971b2d78ff1f4f21f
dawnzzz <635441876@qq.com>
2022-05-24 20:31:57 +0800

    删除临时接口

--- service/favorite.go
+++ service/favorite.go
@@ -18,16 +18,25 @@ func FavoriteAction(userID, videoID uint64) error {
 		if f.IsFavorite == false {
 			// 更新点赞状态
 			f.IsFavorite = true
-			result = global.GVAR_DB.Save(&f)
-			return result.Error
+			err := global.GVAR_DB.Save(&f).Error
+			if err != nil {
+				// 更新点赞状态出错，直接返回
+				return err
+			}
+			return FavoriteCountPlus(videoID) // 点赞数+1
 		}
 	} else {
 		// 在点赞表中新增一个条目
+		f.FavoriteID, _ = global.GVAR_ID_GENERATOR.NextID()
 		f.VideoID = videoID
 		f.UserID = userID
 		f.IsFavorite = true
-		result = global.GVAR_DB.Create(&f)
-		return result.Error
+		err := global.GVAR_DB.Create(&f).Error
+		if err != nil {
+			// 插入出错，直接返回
+			return err
+		}
+		return FavoriteCountPlus(videoID) // 点赞数+1
 	}
 
 	return nil
@@ -44,8 +53,12 @@ func CancelFavorite(userID, videoID uint64) error {
 		if f.IsFavorite == true {
 			// 更新点赞状态，取消点赞
 			f.IsFavorite = false
-			result = global.GVAR_DB.Save(&f)
-			return result.Error
+			err := global.GVAR_DB.Save(&f).Error
+			if err != nil {
+				// 更新出错，直接返回
+				return err
+			}
+			return FavoriteCountMinus(videoID) // 点赞数-1
 		}
 	}
 	// 不存在直接忽略
@@ -56,14 +69,16 @@ func CancelFavorite(userID, videoID uint64) error {
 // GetFavoriteListByUserID 获取用户点赞列表
 func GetFavoriteListByUserID(userID uint64) ([]dao.Video, error) {
 	favoriteList := make([]dao.Favorite, 0, 20)
+	videoDaoList := make([]dao.Video, 0, 20)
 	videoIDList := make([]uint64, 0, 20)
 	global.GVAR_DB.Model(&dao.Favorite{}).Where("user_id = ? and is_favorite = ?", userID, true).Find(&favoriteList)
 	for _, each := range favoriteList {
 		videoIDList = append(videoIDList, each.VideoID)
 	}
 
-	videoDaoList, err := GetVideoListByVideoIDs(videoIDList)
+	err := GetVideoListByIDs(&videoDaoList, videoIDList)
 	if err != nil {
+		fmt.Printf("132456798%#v\n", err)
 		return []dao.Video{}, err
 	}
 	return videoDaoList, nil
@@ -75,45 +90,4 @@ func GetFavoriteStatus(userID, videoID uint64) bool {
 	// 得到结果
 	global.GVAR_DB.Model(&dao.Favorite{}).Where("user_id = ? and video_id = ?", userID, videoID).First(&f)
 	return f.IsFavorite
-}
-
-// GetFavoriteCount 获取视频videoID的点赞数
-func GetFavoriteCount(videoID uint64) int64 {
-	var count int64
-	global.GVAR_DB.Model(&dao.Favorite{}).Where("video_id = ? and is_favorite = ?", videoID, true).Count(&count)
-	return count
-}
-
-func GetVideoListByVideoIDs(videoIDList []uint64) ([]dao.Video, error) {
-	var videoDaoList []dao.Video
-	for _, videoID := range videoIDList {
-		var videoDao dao.Video
-		sqlStr := "select * from video where video_id = ?"
-		if err := global.GVAR_SQLX_DB.Get(&videoDao, sqlStr, videoID); err != nil {
-			return []dao.Video{}, err
-		}
-		videoDaoList = append(videoDaoList, videoDao)
-	}
-
-	return videoDaoList, nil
-}
-
-func GetFollowCount(userID uint64) int64 {
-	var followCount int64
-	sqlStr := "select count(*) from follow where follower_id = ?"
-	if err := global.GVAR_SQLX_DB.Get(&followCount, sqlStr, userID); err != nil {
-		fmt.Println("exec failed, ", err)
-		return -1
-	}
-	return followCount
-}
-
-func GetFollowerCount(userID uint64) int64 {
-	var followCount int64
-	sqlStr := "select count(*) from follow where user_id = ?"
-	if err := global.GVAR_SQLX_DB.Get(&followCount, sqlStr, userID); err != nil {
-		fmt.Println("exec failed, ", err)
-		return -1
-	}
-	return followCount
 }
